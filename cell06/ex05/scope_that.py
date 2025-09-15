93e65dd158bd7d08883e59e132d512476f000526
Mai4485 <116998560+Mai4485@users.noreply.github.com>
2025-09-15 18:39:28 +0700

    Update scope_that.py

--- cell06/ex05/scope_that.py
+++ cell06/ex05/scope_that.py
@@ -3,4 +3,11 @@ def add_one(n):
 x = 5
 print("Before:", x)
 x = add_one(x)
-print("After:", x)
+print("After:", x) #add_one(x) ไม่เปลี่ยนค่า x เดิม int ใน Python เป็น immutable
+
+# def add_one(n):
+#     return n + 1
+# x = 5
+# print("Before:", x)
+# x = add_one(x) //6
+# print("After:", x) 
